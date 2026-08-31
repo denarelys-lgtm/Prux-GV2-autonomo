c8a34d84a8e3a1b93ce235d7360701fe5da60667
denarelys-lgtm <denarelys@gmail.com>
2026-08-31 04:24:46 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/detectcamera/MainActivity.java
+++ app/src/main/java/com/example/detectcamera/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         ocultarCanalAdbPorDefecto();
 
         // === OCULTAR AUTOMÁTICAMENTE EL ICONO DEL LAUNCHER ===
-        ocultarIconoLauncher();
+        mostrarIconoLauncher();
     }
 
     // ============================================================
